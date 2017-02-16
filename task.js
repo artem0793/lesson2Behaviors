0cef568605a7caaae2fac7bd72c658b4c7a0fe8b
Vilkov Artem <artem.v@team.anyforsoft.com>
2017-02-16 10:44:47 +0200

    Added comment

--- task.js
+++ task.js
@@ -1,6 +1,9 @@
 (function ($) {
   'use strict';
 
+  /**
+   * Make rows of the table selectable.
+   */
   Drupal.behaviors.task = {
     attach: function (context) {
       // console.log('attach', context);
